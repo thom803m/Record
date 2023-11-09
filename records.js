b2969454df731d616fc2e167a4f0816ba5875d50
Mammon22 <106878588+Mammon22@users.noreply.github.com>
2023-11-09 14:31:30 +0100

    records

---
+++ records.js
@@ -0,0 +1,19 @@
+Vue.createApp({
+    data(){
+        return{
+            Record:{Title:"",Artist:"",Duration:"",PublicationYear,DownLoads:""}
+            Records=[]
+        }
+    },
+    async GetAllRecords()
+    {
+        console.log("Created Books")
+        this.Records=th
+    }
+    methods:{
+        AllRecords()
+        {
+            
+        }
+    }
+})
